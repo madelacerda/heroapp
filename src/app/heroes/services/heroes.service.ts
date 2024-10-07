735953690a84c28ffa3dcde929b1ed6c1319ad82
matias de la cerda <mdelacerda@ccs.cl>
2024-10-07 08:09:07 -0300

    Add CRUD operations to HeroesService and form to NewPageComponent
    Included methods for adding, updating, and deleting heroes in HeroesService. Integrated FormGroup and FormControl in NewPageComponent to handle hero form data input.

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -10,21 +10,40 @@ import { environments } from '../../../environments/environments';
 export class HeroesService {
   private baseUrl: string = environments.baseURL;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor( private httpClient: HttpClient ) {
+  }
 
   getHeroes(): Observable<Hero[]> {
-    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+    return this.httpClient.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
-  getHeroById(id: string): Observable<Hero | undefined> {
+  getHeroById( id: string ): Observable<Hero | undefined> {
     return this.httpClient
-      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
-      .pipe(catchError((error) => of(undefined)));
+      .get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(catchError(( error ) => of(undefined)));
   }
 
-  getSuggestions(query: string): Observable<Hero[]> {
+  getSuggestions( query: string ): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(
-      `${this.baseUrl}/heroes?q=${query}&_limit=6`
+      `${ this.baseUrl }/heroes?q=${ query }&_limit=6`
     );
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if (hero.id) throw Error('Hero id is required')
+
+    return this.httpClient.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHeroById( hero: Hero ): Observable<boolean> {
+    return this.httpClient.delete(`${ this.baseUrl }/heroes/${ hero.id }`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      );
   }
 }
